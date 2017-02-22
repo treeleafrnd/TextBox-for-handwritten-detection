8d3a2f2c374609914fd4720a1a3e18765b7547f3
Minghui Liao <mhliao@hust.edu.cn>
2017-02-22 21:14:37 +0800

    fix batch size of training codes

--- examples/TextBoxes/train_icdar13.py
+++ examples/TextBoxes/train_icdar13.py
@@ -287,8 +287,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 8
-accum_batch_size = 8
+batch_size = 32
+accum_batch_size = 32
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
